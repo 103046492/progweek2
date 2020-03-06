6ef076518fe01f6bf1b1971c5f355897ebffdae6
SAMIR CRLJENKOVIC <103046492@student.swin.edu.au>
2020-03-06 15:46:31 +1100

    update usernameprog

---
+++ usernameprog/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace usernameprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int username;
+            
+            Console.WriteLine("Enter Username");
+            username = int.Parse(Console.ReadLine());
+
+            
+            if(username = 103046492)
+                Console.WriteLine("Enter Password");
+            else
+                Console.WriteLine("Wrong User");
+            
+            int password;
+            
+            passwrod = int.Parse(Console.ReadLine());
+            if(password = 123456)
+                Console.WriteLine("Login Successful");
+            else 
+                Console.WriteLine("Login Unsuccessful");
+            Console.ReadLine();
+        }
+    }
+}
